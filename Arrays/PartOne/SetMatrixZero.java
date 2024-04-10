778cb5ccef6a444317c8b6ba2442d7af5bbd0e44
Prashant Singh <singh.p@novelvox.com>
2024-04-10 13:12:22 +0530

    initial setup

---
+++ Arrays/PartOne/SetMatrixZero.java
@@ -0,0 +1,13 @@
+package Arrays.PartOne;
+
+/**
+ * SetMatrixZero
+ */
+public class SetMatrixZero {
+
+    public static void main(String[] args) {
+
+        int [][]matrix = {{1,1,1},{1,0,1},{1,1,1}};
+        
+    }
+}
