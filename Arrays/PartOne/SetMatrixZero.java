22ea9d32232112f626e9b25b41cfcbd3fd52a52b
Prashant Singh <singh.p@novelvox.com>
2024-04-11 22:32:58 +0530

    setMatrix Zero

--- Arrays/PartOne/SetMatrixZero.java
+++ Arrays/PartOne/SetMatrixZero.java
@@ -7,7 +7,66 @@ public class SetMatrixZero {
 
     public static void main(String[] args) {
 
-        int [][]matrix = {{1,1,1},{1,0,1},{1,1,1}};
+        // int [][]matrix = {{1,1,1},{1,0,1},{1,1,1}};
+        int [][]matrix = {{0,1}};
+        // int [][]matrix = {{0,1,2,0},{3,4,5,2},{1,3,1,5}};
+
+
+
+        int row = matrix.length;
+        int column = matrix[0].length;
         
+        // traverse the array -> iterate through row and then column 
+        
+        bruteForceApproach(matrix, row, column);
+        
+    }
+
+    private static void bruteForceApproach(int[][] matrix, int row, int column) {
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < column; j++) {
+
+                // System.out.print(matrix[i][j] + " ");
+                if(matrix[i][j] == 0){
+
+                   //mark all row as -1 except that cell
+                   //mark all colum as -1 except that cell
+                //    markRows(i);
+                //    markColumn(j);
+                for(int k=0;k<row;k++){
+                    if(matrix[k][j] !=0){
+                        matrix[k][j] = -1;
+                    }
+                }
+                for (int j2 = 0; j2 < column; j2++) {
+                    if(matrix[i][j2]!=0){
+                        matrix[i][j2]= -1;
+                    }
+                    
+                }
+
+                }
+                
+            }
+            // System.out.println();
+            
+        }
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < column; j++) {
+                if(matrix[i][j]<0){
+                    matrix[i][j] = 0;
+                }
+                
+            }
+            
+        }
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < column; j++) {
+                System.out.print(matrix[i][j]);
+                
+            }
+            System.out.println();
+            
+        }
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # StriverSheet
 StriverSheet for DSA
+https://takeuforward.org/interviews/strivers-sde-sheet-top-coding-interview-problems/
