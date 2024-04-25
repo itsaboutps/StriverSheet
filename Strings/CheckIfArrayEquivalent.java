d75effeb1ffa0f1132681a66d1f9549fb26aa729
Prashant <psprashantsingh91@gmail.com>
2024-04-26 02:38:38 +0530

    git array string

---
+++ Strings/CheckIfArrayEquivalent.java
@@ -0,0 +1,20 @@
+package Strings;
+
+public class CheckIfArrayEquivalent {
+    public static void main(String[] args) {
+        
+    }
+}
+
+
+class Solution {
+    public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+
+return String.join("", word1).equals(String.join("", word2));
+
+
+
+
+        
+    }
+}
