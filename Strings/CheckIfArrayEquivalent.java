e095f7cca4887a94563b6d3d8353982f60cd5a95
Prashant <psprashantsingh91@gmail.com>
2024-05-03 01:19:36 +0530

    dsa learnings

--- Strings/CheckIfArrayEquivalent.java
+++ Strings/CheckIfArrayEquivalent.java
@@ -6,7 +6,7 @@ public class CheckIfArrayEquivalent {
     }
 }
 
-
+//! how to join the string and how to join the elements of array using String.join
 class Solution {
     public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
 
